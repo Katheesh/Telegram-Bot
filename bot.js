dd6f6c8827d04b6950994b009592817f564e7633
katheesh <katheeskumar@outlook.com>
2021-07-02 20:49:34 +0530

    added new gif and messages

--- bot.js
+++ bot.js
@@ -1,16 +1,21 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true});
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
+
+const express = require('express')
+const app = express()
+const port = 3000
+
 
 // ---------- LOGGIN FEATURES ----------
 var fs = require('fs');
 var util = require('util');
-var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'a'});
+var log_file = fs.createWriteStream(__dirname + '/debug.log', { flags: 'a' });
 var log_stdout = process.stdout;
 
-console.log = function(d) { //
-  log_file.write(util.format(d) + '\n');
-  log_stdout.write(util.format(d) + '\n');
+console.log = function (d) { //
+	log_file.write(util.format(d) + '\n');
+	log_stdout.write(util.format(d) + '\n');
 };
 // ---------- LOGGIN FEATURES END ----------
 
@@ -21,47 +26,48 @@ var fastAnswers = JSON.parse(fs.readFileSync('./answers.json', 'utf8'));
 console.log("[DEBUG] Bot is starting...");
 
 
-bot.on('message', (msg) => {  
-     //bot.sendMessage(msg.chat.id, "Hello dear user"); 
-	 //console.log(msg);
-	 let date = new Date(msg.date * 1000);
-	 let timestamp = date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear() + "@" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-	 
-	
+bot.on('message', (msg) => {
+	//bot.sendMessage(msg.chat.id, "Hello dear user"); 
+	//console.log(msg);
+	let date = new Date(msg.date * 1000);
+	let timestamp = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + "@" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+
 	let msgFromInfo = "";
-	if(msg.chat.type == "private"){
+	if (msg.chat.type == "private") {
 		msgFromInfo = msg.from.first_name + "(" + msg.from.id + ")";
-	}else if(msg.chat.type == "group"){
+	} else if (msg.chat.type == "group") {
 		msgFromInfo = msg.from.first_name + "(" + msg.from.id + "/" + msg.chat.title + ")";
 	}
-	
-	 console.log("[INFO](" + timestamp + ") Msg from " + msgFromInfo + ": " + msg.text);
-	 
-	 if(msg.text != null){
+
+	console.log("[INFO](" + timestamp + ") Msg from " + msgFromInfo + ": " + msg.text);
+
+
+	if (msg.text != null) {
 		let mex = controlMessage(msg.text);
-		if(mex != null){
+		if (mex != null) {
 			//bot.sendMessage(msg.chat.id, mex);
-					
-			if(mex.type == "text"){
+
+			if (mex.type == "text") {
 				bot.sendMessage(msg.chat.id, mex.reply);
-			}else if(mex.type == "image"){
-				if(mex.reply.includes("gif")){
+			} else if (mex.type == "image") {
+				if (mex.reply.includes("gif")) {
 					bot.sendVideo(msg.chat.id, mex.reply);
-				}else{
+				} else {
 					bot.sendPhoto(msg.chat.id, mex.reply);
 				}
-			}else if(mex.type == "audio"){
+			} else if (mex.type == "audio") {
 				//bot.sendAudio(msg.chat.id, mex.reply, "Xfox Assistant Bot", "Title", "Caption");
 				bot.sendVoice(msg.chat.id, mex.reply);
-			}else if(mex.type == "function"){
+			} else if (mex.type == "function") {
 				mex.reply(bot); //let function handle the answer
 			}
-		 }
-	 }
+		}
+	}
 });
 
-function controlMessage(message){
-	
+function controlMessage(message) {
+
 	/*
 	for (const [key, phrases] of Object.entries(fastAnswers)) { //Check for fast answers
 		if(message.toLowerCase().includes(key)){
@@ -70,23 +76,31 @@ function controlMessage(message){
 		}
 	}
 	*/
-	
+
 	let found = null;
 	//TODO: Should substitute forEach with for (const [triggers, oneFastAnswer] of Object.entries(fastAnswers)), in order to use return inside the loop
-	fastAnswers.forEach(function(fastAnswer){ //For every fast answer
-		fastAnswer.triggers.forEach(function(trigger){ //Check among all triggers
-			let regex = new RegExp("\\b"+trigger+"\\b", "gi");//Search global and case insenstive
+	fastAnswers.forEach(function (fastAnswer) { //For every fast answer
+		fastAnswer.triggers.forEach(function (trigger) { //Check among all triggers
+			let regex = new RegExp("\\b" + trigger + "\\b", "gi");//Search global and case insenstive
 			let regexResult = message.match(regex);
-			
+
 			//console.log("Regex result: " + regexResult);
-			
-			if((regexResult != null) && !found){ //If RegEx matches and wasn't previously found
+
+			if ((regexResult != null) && !found) { //If RegEx matches and wasn't previously found
 				let rnd = Math.floor((Math.random() * (fastAnswer.replies.length)) + 0);
 				found = fastAnswer.replies[rnd];
 				//return fastAnswer.replies[rnd]; //Can't do this with forEach (ahw man, that sucks), see comment above, substitute forEach with for
-			}	
+			}
 		});
 	});
-	
+
 	return found;
 }
+
+app.get('/', (req, res) => {
+	res.send("This is TamilBot")
+})
+
+app.listen(8000, () => {
+	console.log(`App Started at https://tamilrobot.herokuapp.com/`)
+})
